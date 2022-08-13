4bee16dc49de9531be4b9b336348018f1252c485
Henri Iser <iserhenri@gmail.com>
2022-08-13 11:10:28 +0200

    Additional Functions in init (Nice to have)

--- pypatchify/__init__.py
+++ pypatchify/__init__.py
@@ -105,3 +105,43 @@ def unpatchify_from_batches(t:Union[ndarray, Any], unpatched_sizes:Sequence[int]
         return pt.unpatchify_from_batches(t, unpatched_sizes, batch_dim)
     # fallback to numpy
     return np.unpatchify_from_batches(t, unpatched_sizes, batch_dim)
+
+def collapse_dims(cls, t:Union[ndarray, Any], dims:Sequence[int], target_dim:int =0) -> Union[ndarray, Any]:
+    """ Collapse multiple dimensions of a given tensor 
+
+        Args:
+            t (Tensor): input tensor
+            dims (Sequence[int]): dimensions to collapse in the input tensor
+            target_dim (int): dimension into which to collapse the given dimensions. Defaults to 0.
+    
+        Returns:
+            collapsed (Tensor): tensor with dimensions collapsed into target dimension
+    """
+    # use pytorch if given
+    if IS_TORCH_AVAILABLE and isinstance(t, torch.Tensor):
+        return pt.collapse_dims(t, dims, target_dim)
+    # fallback to numpy
+    return np.collapse_dims(t, dims, target_dim)
+
+def sliding_window(cls, t:Union[ndarray, Any], window_dims:Sequence[int], strides:Sequence[int]) -> Union[ndarray, Any]:
+    """ Sliding Window view on given tensor
+        
+        Args:
+            t (Tensor): 
+                tensor object of dimension k on which to apply the sliding window view
+            window_dims (Sequence[int]): 
+                dimensions of sliding window. Window dimensionality n must be <= k.
+                Windows will be applied to the last n dimensions of the input tensor.
+            strides (Sequence[int]): 
+                strides (i.e. step size) of sliding window in each dimension
+
+        Returns:
+            windows (Tensor): 
+                tensor containing sliding windows in shape (d_1, ..., d_(k-n), p_1, ..., p_n, *window_dims)
+                where pi is the number of sliding windows in the i-th dimension
+    """
+    # use pytorch if given
+    if IS_TORCH_AVAILABLE and isinstance(t, torch.Tensor):
+        return pt.sliding_window(t, window_dims, strides)
+    # fallback to numpy
+    return np.sliding_window(t, window_dims, strides)
